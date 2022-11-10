0528dcba79f21c33a4706c87f08908278f86f9dc
Nomen <justnomen@outlook.com>
2022-11-10 19:35:19 +0000

    refactor image sliders

--- FE/src/components/Slider/Slider.tsx
+++ FE/src/components/Slider/Slider.tsx
@@ -1,9 +1,12 @@
+import {useDevice} from "hooks/useDevice";
 import React from "react";
 import {Carousel} from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import * as S from "./Slider.styles";
 
 export const Slider = () => {
+    const {isDesktop} = useDevice();
+
     return (
         <Carousel
             autoPlay
@@ -13,11 +16,10 @@ export const Slider = () => {
             dynamicHeight={true}
             showArrows={false}
         >
-            <div><S.Img src="sliders/1.jpg" alt="" width={600} height={600}/></div>
-            <div><S.Img src="sliders/2.jpg" alt="" width={600} height={600}/></div>
-            <div><S.Img src="sliders/3.jpg" alt="" width={600} height={600}/></div>
-            <div><S.Img src="sliders/6.jpg" alt="" width={600} height={600}/></div>
+            <div><S.Img src="sliders/1.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
+            <div><S.Img src="sliders/2.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
+            <div><S.Img src="sliders/3.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
+            <div><S.Img src="sliders/6.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
         </Carousel>
-
     )
 }
