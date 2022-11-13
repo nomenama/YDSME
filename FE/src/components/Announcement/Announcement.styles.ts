474412aa4f7895a06164699951b973406224cae1
Nomen <justnomen@outlook.com>
2022-11-13 10:30:56 +0000

    refactor announcement marquee

--- FE/src/components/Announcement/Announcement.styles.ts
+++ FE/src/components/Announcement/Announcement.styles.ts
@@ -1,43 +1,16 @@
 import styled from "styled-components";
 import {theme} from "../../styles/theme";
 
-export const Container = styled.div`
-  position: relative;
-  display: block;
-  width: 100%;
-  height: 50px;
-  overflow: hidden;
-`
-
-export const Marquee = styled.div`
-  position: absolute;
-  display: block;
-  margin: auto auto;
-  white-space: nowrap;
-  overflow: hidden;
-  min-width: 100%;
-  height: 100%;
+export const AnnouncementContainer = styled.div`
+  padding-right: 30vw;
 `
 
 export const Span = styled.span`
-  display: inline-block;
-  padding-left: 100%;
   font-family: 'Roboto', sans-serif;
   text-align: center;
   -webkit-text-stroke: 1px ${theme.colors.primary};
   white-space: nowrap;
-  min-width: 100%;
   height: 100%;
   line-height: 50px;
   font-size: 2rem;
-  animation: marquee 20s linear infinite;
-  
-  @keyframes marquee {
-    0% {
-      transform: translate(0, 0);
-    }
-    100% {
-      transform: translate(-100%, 0);
-    }
-  }
 `
--- FE/src/components/Announcement/Announcement.tsx
+++ FE/src/components/Announcement/Announcement.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import {Container, Marquee, Span} from "./Announcement.styles";
+import Marquee from "react-fast-marquee";
+import {AnnouncementContainer, Span} from "./Announcement.styles";
 
 const Announcement = () => {
-    const announcements = ["Welcome to York District Society of Model Engineering", "Follow us on Facebook for the latest updates"]
+    const announcements = ["Welcome to York District Society of Model Engineering - Follow us on Facebook for latest updates", "Don't miss out Christmas Day celebration. Open to public. Book now!"]
 
     return (
-        <Container>
-            <Marquee>
-                {announcements.map((announcement, index) => {
-                    return (
-                        <Span key={announcement + index}>{announcement}</Span>
-                    )
-                })}
-            </Marquee>
-
-        </Container>
+        <Marquee speed={60} gradient={false} pauseOnHover={true} delay={1}>
+            {announcements.map((announcement, index) => {
+                return (
+                    <AnnouncementContainer key={announcement + index}>
+                        <Span>{announcement}</Span>
+                    </AnnouncementContainer>
+                )
+            })}
+        </Marquee>
     )
 }
 
