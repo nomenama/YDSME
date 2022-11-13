0fb65a858f2147e5fcbbc0eb4e8bdc84dca4c966
Nomen <justnomen@outlook.com>
2022-11-13 16:04:05 +0000

    refactor navigation

--- FE/src/components/Layout/Layout.tsx
+++ FE/src/components/Layout/Layout.tsx
@@ -4,41 +4,33 @@ import Header from 'components/Header/Header';
 import {MainNavigation, MemberNavigation} from 'components/Header/Navigation';
 import React from 'react';
 import {Outlet} from "react-router-dom";
-import {ThemeProvider} from "styled-components";
-import GlobalStyles from 'styles/Global';
-import {theme} from 'styles/theme';
 import {useDevice} from "../../hooks/useDevice";
 
 export const DefaultLayout = () => {
     const {isDesktop} = useDevice();
 
     return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyles/>
+        <>
             <Header navigations={MainNavigation} includeLoginButton={true}/>
             {!isDesktop && <Announcement/>}
             <Outlet/>
             <Footer/>
-        </ThemeProvider>
+        </>
     );
 };
 
 export const MemberLayout = () => {
     return (
-        <ThemeProvider theme={theme}>
+        <>
             <Header navigations={MemberNavigation} includeLogoutButton={true}/>
-            <GlobalStyles/>
             <Outlet/>
             <Footer/>
-        </ThemeProvider>
+        </>
     );
 };
 
 export const UnhandledLayout = () => {
     return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyles/>
-            <Outlet/>
-        </ThemeProvider>
+        <Outlet/>
     );
 };
--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {UserProvider} from "./context/UserProvider";
+import {ThemeProvider} from 'styled-components';
+import {theme} from 'styles/theme';
+import GlobalStyles from 'styles/Global';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <UserProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/*" element={<App/>}/>
-                </Routes>
-            </BrowserRouter>
-        </UserProvider>
+        <ThemeProvider theme={theme}>
+            <GlobalStyles/>
+            <UserProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/*" element={<App/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </UserProvider>
+        </ThemeProvider>
     </React.StrictMode>
 );
