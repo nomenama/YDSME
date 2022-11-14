f0f3f2c832716d8efe2172bb7c2e18ca22da9bd5
Nomen <justnomen@outlook.com>
2022-11-14 01:49:30 +0000

    refactor public content context

---
+++ FE/src/context/ContentProvider.tsx
@@ -0,0 +1,39 @@
+import React, {createContext, useEffect, useState} from "react";
+import {getAnnouncement} from "../api/api";
+
+interface ContentProps {
+    announcements: string[] | undefined;
+}
+
+const ContentContext = createContext<any>({});
+
+export const ContentProvider = ({children}: any) => {
+    const [announcements, setAnnouncements] = useState<string[]>([]);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+        const asyncProcess = async () => {
+            try {
+                const {status, data} = await getAnnouncement(signal);
+                console.log(data)
+                if (status === 200) {
+                    setAnnouncements(data as string[]);
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        void asyncProcess();
+
+        return () => controller.abort();
+    }, []);
+
+    return (
+        <ContentContext.Provider value={{announcements}}>
+            {children}
+        </ContentContext.Provider>
+    )
+};
+
+export default ContentContext;
