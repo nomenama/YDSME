ac2fec8d4b7f07b233eabecbfe6b4fef1a6b8579
Nomen <justnomen@outlook.com>
2022-11-18 21:05:28 +0000

    set cookies header

--- BE/src/allowedCors/index.js
+++ BE/src/allowedCors/index.js
@@ -1,8 +1,4 @@
 export const allowedCors = {
-	origin: [
-		process.env.NODE_ENV === "production"
-			? process.env.REQUEST_ORIGIN
-			: "http://localhost:3000"
-	],
+	origin: true,
 	credentials: true
 };
