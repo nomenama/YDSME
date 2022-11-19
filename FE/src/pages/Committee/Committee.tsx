e71e3a9fb7fee0a15799f995827b001c363479a3
Nomen <justnomen@outlook.com>
2022-11-19 01:41:57 +0000

    update page title and metadata

--- FE/src/pages/Committee/Committee.tsx
+++ FE/src/pages/Committee/Committee.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import {H1, InnerContainer, PageContainer} from "../../common/index.styles";
+import {GrVmMaintenance} from "react-icons/gr";
+import {useTheme} from "styled-components";
 
 export const Committee = () => {
+    const theme = useTheme();
+
     return (
         <PageContainer>
-            <InnerContainer justifyContent="center">
-                <H1>Contact</H1>
+            <InnerContainer justifyContent="center" alignItems="center">
+                <H1>Coming soon...</H1>
+                <GrVmMaintenance size={100} style={{color: theme.buttons.primary}}/>
             </InnerContainer>
         </PageContainer>
     )
