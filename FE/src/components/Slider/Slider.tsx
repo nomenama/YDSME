488e8cfa3482cef7a05529226ea35b96d12f99d6
Nomen <justnomen@outlook.com>
2022-11-26 14:56:16 +0000

    set slider image height

--- FE/src/components/Slider/Slider.tsx
+++ FE/src/components/Slider/Slider.tsx
@@ -5,7 +5,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import * as S from "./Slider.styles";
 
 export const Slider = () => {
-    const {isDesktop} = useDevice();
+    const {deviceWidth} = useDevice();
 
     return (
         <Carousel
@@ -16,15 +16,15 @@ export const Slider = () => {
             dynamicHeight={true}
             showArrows={false}
         >
-            <div><S.Img src="sliders/1.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
-            <div><S.Img src="sliders/2.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
-            <div><S.Img src="sliders/3.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
-            <div><S.Img src="sliders/4.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
-            <div><S.Img src="sliders/5.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
-            <div><S.Img src="sliders/6.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
-            <div><S.Img src="sliders/7.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
-            <div><S.Img src="sliders/8.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
-            <div><S.Img src="sliders/11.jpg" alt="" width={600} height={isDesktop ? 600 : 400}/></div>
+            <div><S.Img src="sliders/1.jpg" alt="" width={600} height={deviceWidth > 1200 ? 600 : 400}/></div>
+            <div><S.Img src="sliders/2.jpg" alt="" width={600} height={deviceWidth > 1200 ? 600 : 400}/></div>
+            <div><S.Img src="sliders/3.jpg" alt="" width={600} height={deviceWidth > 1200 ? 600 : 400}/></div>
+            <div><S.Img src="sliders/4.jpg" alt="" width={600} height={deviceWidth > 1200 ? 600 : 400}/></div>
+            <div><S.Img src="sliders/5.jpg" alt="" width={600} height={deviceWidth > 1200 ? 600 : 400}/></div>
+            <div><S.Img src="sliders/6.jpg" alt="" width={600} height={deviceWidth > 1200 ? 600 : 400}/></div>
+            <div><S.Img src="sliders/7.jpg" alt="" width={600} height={deviceWidth > 1200 ? 600 : 400}/></div>
+            <div><S.Img src="sliders/8.jpg" alt="" width={600} height={deviceWidth > 1200 ? 600 : 400}/></div>
+            <div><S.Img src="sliders/11.jpg" alt="" width={600} height={deviceWidth > 1200 ? 600 : 400}/></div>
         </Carousel>
     )
 }
